5b561cf9ca13cbfc5d92902863777dfa91363dfb
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-04-07 09:54:00 +0300

    Simplified onReset method for replicanti
    Refactored onReset method to always reset replicanti to 1.

--- js/contents/function.js
+++ js/contents/function.js
@@ -214,12 +214,7 @@ const FORMS = {
                 }
             },
             onReset() {
-                if(ACHS.has(53)) {
-                    player.inf.replicanti = player.inf.replicanti.div(this.req()) 
-                }
-                else {
-                    player.inf.replicanti = E(1)
-                
+                player.inf.replicanti = E(1)       
             },
             effect(x=player.inf.comp) {
                 let ret = {}
